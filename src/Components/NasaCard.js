ced04c5ef03b94d446e84ca79be5d600f290e3e5
Naomi <naomipriest@rocketmail.com>
2020-02-19 18:46:27 -0500

    Finished for today's half of the project

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -3,12 +3,12 @@ import React from "react";
 const NasaCard = props => {
   return (
     <div key = {props.id} className="img-list">
-      <h2>Image Title: {props.title} </h2>
+      <h2 className = "title">{props.title} </h2>
         <img src = {props.image} alt = "nasa pictures" />
 
-      <div>
-        <p>Date: {props.date}</p>
-        <p>Explanation: {props.explanation}</p>
+      <div className = "dateanddata">
+        <p>{props.date}</p>
+        <p>{props.explanation}</p>
       </div>
     </div>
   );
