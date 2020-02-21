38bd7a07664c70c1e686b7f6a2b31007eef6d41b
Naomi <naomipriest@rocketmail.com>
2020-02-20 21:58:13 -0500

    Saving my finished day two project

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -1,16 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+
+
+const StyledDiv = styled.div`
+  background-color: lightgray;
+
+
+`;
+
+const StyledH2 = styled.h2`
+  padding:20px;
+  border: 2px dashed red;
+  background: lightgray;
+  color: blue;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  margin: 40px;
+`;
+
+const StyledImg = styled.img`
+  max-width:80%;
+  border-radius: 10px;
+
+`;
+
+const StyledP = styled.p`
+  display:flex;
+  justify-content:center;
+  align-content:center;
+  margin:40px;
+  border:2px dashed blue;
+  color: red;
+  padding: 20px;
+
+`;
+
+const StyledDate = styled.p`
+  display:flex;
+  align-content:center;
+  justify-content:center;
+  margin: 40px;
+  border: 2px dashed red;
+  padding: 20px;
+  
+
+`;
 
 const NasaCard = props => {
   return (
-    <div key = {props.id} className="img-list">
-      <h2 className = "title">{props.title} </h2>
-        <img src = {props.image} alt = "nasa pictures" />
+    <StyledDiv key = {props.id} className="img-list">
+      <StyledH2 className = "title">{props.title} </StyledH2>
+        <StyledImg src = {props.image} alt = "nasa pictures" />
 
       <div className = "dateanddata">
-        <p>{props.date}</p>
-        <p>{props.explanation}</p>
+        <StyledDate>{props.date}</StyledDate>
+        <StyledP>{props.explanation}</StyledP>
       </div>
-    </div>
+    </StyledDiv>
   );
 };
 export default NasaCard;
